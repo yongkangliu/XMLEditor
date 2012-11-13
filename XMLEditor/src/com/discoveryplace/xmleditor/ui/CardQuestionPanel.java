0af5d8624b7cc875be5c323ccd84a51a19e1f9d8
yongkangliu <yongkangliu@gmail.com>
2012-11-12 20:54:43 -0500

    ok

--- XMLEditor/src/com/discoveryplace/xmleditor/ui/CardQuestionPanel.java
+++ XMLEditor/src/com/discoveryplace/xmleditor/ui/CardQuestionPanel.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -18,10 +19,13 @@ public class CardQuestionPanel extends CardNodePanel {
     private static final long serialVersionUID = -3942735705442838426L;
     public static String QUESTION = "question";
 
+    private static String TEXT = "text";
+    private static String IMAGE = "image";
+    private static String VIDEO = "video";
     private static CardQuestionPanel instance = new CardQuestionPanel();
 
-    private JTextField questionType = new JTextField();
     private JTextField questionURL = new JTextField();
+    private JComboBox<String> questionType = new JComboBox<String>(new String[] { TEXT, IMAGE, VIDEO });
 
     private CardQuestionPanel() {
         // Fields for Question information
@@ -29,14 +33,14 @@ public class CardQuestionPanel extends CardNodePanel {
         JLabel line20Label = new JLabel("Question Type:");
         line20Label.setHorizontalAlignment(SwingConstants.RIGHT);
         line20.add(line20Label);
-        line20.add(questionType);
+        line20.add(this.questionType);
         this.add(line20);
 
         JPanel line21 = new JPanel(new GridLayout(1, 2));
         JLabel line21Label = new JLabel("Image/Video URL:");
         line21Label.setHorizontalAlignment(SwingConstants.RIGHT);
         line21.add(line21Label);
-        line21.add(questionURL);
+        line21.add(this.questionURL);
         this.add(line21);
 
         JButton questionButton = new JButton("save");
@@ -59,13 +63,13 @@ public class CardQuestionPanel extends CardNodePanel {
 
     public void showNodeValue(TreeNode node) {
         super.showNodeValue(node);
-        this.questionType.setText(((TreeNodeQuestion) node).getQuestionType());
+        this.questionType.setSelectedItem(((TreeNodeQuestion) node).getQuestionType());
         this.questionURL.setText(((TreeNodeQuestion) node).getUrl());
     }
 
     protected void setNodeValue() {
         super.setNodeValue();
-        ((TreeNodeQuestion) this.treeNode).setQuestionType(this.questionType.getText());
+        ((TreeNodeQuestion) this.treeNode).setQuestionType((String) this.questionType.getSelectedItem());
         ((TreeNodeQuestion) this.treeNode).setUrl(this.questionURL.getText());
     }
 }
